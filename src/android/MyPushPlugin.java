66bb26af232044a870d8b5ab32ebc3f91bf38155
vcorreaj <vcorrea@medianet.com.ve>
2026-03-18 09:13:11 -0400

    deudas

--- src/android/MyPushPlugin.java
+++ src/android/MyPushPlugin.java
@@ -7,15 +7,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
 
 import android.util.Log;
+import android.content.Context;
 
 public class MyPushPlugin extends CordovaPlugin {
     private static final String TAG = "MyPushPlugin";
     private static CallbackContext notificationCallbackContext;
     private static CallbackContext tokenCallbackContext;
+    
+    @Override
+    protected void pluginInitialize() {
+        super.pluginInitialize();
+        
+        // 🔥 Inicializar Firebase automáticamente
+        Context context = this.cordova.getActivity().getApplicationContext();
+        if (FirebaseApp.getApps(context).isEmpty()) {
+            FirebaseApp.initializeApp(context);
+            Log.d(TAG, "Firebase inicializado automáticamente");
+        }
+    }
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
